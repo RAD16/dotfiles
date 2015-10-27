ce3234ed1d26322f63f4048c945d7a3ae4b5d73a
Ryan Donnelly <herbivore.linux@gmail.com>
2015-10-27 08:52:27 -0400

    tmux bootstrap created and working

--- tmux/tm.sh
+++ tmux/tm.sh
@@ -8,6 +8,12 @@ tmux new-window -t $1:1 -n 'code'
 tmux new-window -t $1:2 -n 'bash'
 tmux new-window -t $1:3 -n 'bash'
 
+# Create and size panes
 tmux select-window -t $1:1
+tmux split-window -v -p 10 -t $1
+tmux select-pane -t 0
+tmux split-window -h -p 30 -t $1
+tmux select-pane -t 0
+
 tmux attach -t ide
 
