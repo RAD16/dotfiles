914873bd257745c6caa574606aeb60d67b57586c
Ryan Donnelly <herbivore.linux@gmail.com>
2015-10-29 15:23:34 -0400

    tm.sh - reorg boot defaults:labels, windows

--- tmux/tm.sh
+++ tmux/tm.sh
@@ -8,21 +8,13 @@
 newide() 
 {
 			tmux new-session -s $1 -d 
-<<<<<<< HEAD
-			tmux new-window -t $1:1 -n 'code' -c ~/buildd/
-			tmux new-window -t $1:2 -n 'bash' -c ~/buildd/
-			tmux new-window -t $1:3 -n 'NOTES' -c ~/buildd/
+			tmux new-window -t $1:1 -n 'IDE' -c ~/buildd/
+			tmux new-window -t $1:2 -n 'NOTES' -c ~/buildd/
 			tmux select-window -t $1:1 # Create and size panes
-=======
-			tmux new-window -t $1:1 -n 'bash' -c ~/buildd/
-			tmux new-window -t $1:2 -n 'IDE' -c ~/buildd/
-			tmux select-window -t $1:2 # Create and size panes
->>>>>>> f46bf48047caa1ca5931b872e1f58216dd0e8710
 			tmux split-window -v -p 20 -t $1
 			tmux select-pane -t 0
 			tmux split-window -h -p 30 -t $1
-			tmux select-pane -t 0 # Neccessary line? 
-			tmux select-window -t $1:1
+			tmux select-window -t $1:0
 			tmux attach -t "$1" 
 	} 
 
